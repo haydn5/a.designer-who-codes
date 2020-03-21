b0da71b86d3cc39e13daa70af5cef0d592fa9249
haydn5 <haydn@nautilusdesigns.com>
2020-03-21 08:13:19 -0400

    fixed analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,21 @@ module.exports = {
     siteUrl: `https://adesignerwhocodes.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-161489374-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        cookieDomain: "https://adesignerwhocodes.com",
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
