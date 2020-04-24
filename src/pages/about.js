6b618ea55275d94e202ed8abf6568292811c439a
haydn5 <haydn@nautilusdesigns.com>
2020-04-24 10:39:25 -0400

    changed the form around

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,19 +8,7 @@ const About = (props) => (
     <section id={props.sectionName}>
         <Container>
             <h2 className="text-center">About Haydn S. Adams, BFA</h2>
-            {/* <script src="https://f.convertkit.com/ckjs/ck.5.js" />
-      <form action="https://app.convertkit.com/forms/1341543/subscriptions" className="seva-form formkit-form" method="post" data-sv-form="1341543" data-uid="b3d7490e6f" data-version="5" data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;https://adesignerwhocodes.com/#patreon&quot;,&quot;action&quot;:&quot;message&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:false},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}"  min-width="400 500 600 700 800">
-          <div data-style="clean">
-            <ul className="formkit-alert formkit-alert-error" data-element="errors" data-group="alert"></ul>
-            <div data-element="fields" data-stacked="false" className="seva-fields formkit-fields">
-                <div className="formkit-field">
-                    <input className="formkit-input" aria-label="Your first name" name="fields[first_name]" placeholder="Your first name" type="text" />
-                </div>
-                <div className="formkit-field">
-                    <input className="formkit-input" name="email_address" placeholder="Your email address" required="" type="email" />
-                </div>
-                <button data-element="submit" className="formkit-submit formkit-submit" >
-                    <div className="formkit-spinner"><div></div><div></div><div></div></div><span>Subscribe</span></button></div></div></form> */}
+            
             <Row>
                 <Col lg={4}>
                     <p><Image src={haydnPhoto} alt="Photo of Haydn" fluid rounded /></p>
--- src/pages/overview.js
+++ src/pages/overview.js
@@ -10,7 +10,7 @@ import computerAfter from "../images/computer-after.jpg"
 
 
 // import Scrollspy from 'react-scrollspy'
-import { Container, Image, Row, Col } from "react-bootstrap";
+import { Container, Image, Row, Col, Form } from "react-bootstrap";
 
 const AboutPage = (props) => (
     <section id={props.sectionName}>
@@ -29,6 +29,36 @@ const AboutPage = (props) => (
                     <Image src={computerAfter} alt="Computer After" fluid />
                 </Col>
             </Row>
+            <hr />
+            <h2>Sign Up for our monthly email</h2>
+
+
+            <script src="https://f.convertkit.com/ckjs/ck.5.js" />
+            <Form inline action="https://app.convertkit.com/forms/1341543/subscriptions" className="seva-form formkit-form" method="post" data-sv-form="1341543" data-uid="b3d7490e6f" data-version="5" data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;https://adesignerwhocodes.com/#patreon&quot;,&quot;action&quot;:&quot;message&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://convertkit.com?utm_source=dynamic&amp;utm_medium=referral&amp;utm_campaign=poweredby&amp;utm_content=form&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:true},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}"  min-width="400 500 600 700 800">
+            <div data-style="clean">
+
+
+            <Form.Group 
+                className="formkit-alert formkit-alert-error" 
+                data-group="alert"
+                data-element="fields" data-stacked="false"
+            >
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+            <Form.Group 
+                data-group="alert"
+                data-element="fields" data-stacked="false"
+            >
+                <Form.Control placeholder="Your first name" type="text" name="fields[first_name]" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+                <button data-element="submit" className="formkit-submit formkit-submit" >
+                    <span>Subscribe</span></button></div></Form>
         </Container>
         
     </section>
