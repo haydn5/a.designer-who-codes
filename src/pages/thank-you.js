d8dc99f15e0acd5c8ea389de0343c0da816583f2
haydn5 <haydn@nautilusdesigns.com>
2020-04-24 12:11:02 -0400

    added a thank you page

---
+++ src/pages/thank-you.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="Thank you!" />
+    <h1>Thanks</h1>
+    <p>Thank you for subscribing</p>
+  </Layout>
+)
+
+export default NotFoundPage
